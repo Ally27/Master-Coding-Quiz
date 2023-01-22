a38002f6e170156a97a1a32be231090579c346ee
Allyson Gonzales <Allysonmg21@gmail.com>
2023-01-21 21:22:57 -0800

    attempting timer count down and questions tramsition

--- assets/Javascript/script.js
+++ assets/Javascript/script.js
@@ -1,26 +1,83 @@
-var timerEl = document.getElementById("timer");
-var correctAnswerEL = document.getElementById("correctAnswer");
-var wrongAnswerEL = document.querySelector(".wrong");
 var startButton = document.getElementById("start-btn");
+var wrongAnswerEL = document.querySelector(".wrong");
 var btn1 = document.getElementById("btn1");
 var btn2 = document.getElementById("btn2");
 var btn3 = document.getElementById("btn3");
 var btn4 = document.getElementById("btn4");
+var questionEl = document.getElementById("question");
+var timerEl = document.getElementById("timer");
+var correctAnswerEL = document.getElementById("correctAnswer");
+
+var timeLeft;
+var timerInterval;
+var index;
+
+let shuffleQuestions, currentQuestion;
+
+startButton.addEventListener("click", startGame);
 
 function startGame() {
   console.log("start");
   startButton.classList.add("hide");
-  correctAnswerEL.classList.remove("hide");
+  shuffleQuestions = questions.sort(() => Math.random() - 0.5);
+  currentQuestion = 0;
+  box.classList.remove("hide");
+  startTimer();
+  nextQuestion();
 }
 
-(function () {
-  var sec = 50;
-  timerEl = setInterval(() => {
-    timerEl.textContent = "50:" - sec;
-    sec--;
-  }, 1000);
-})();
+function renderQuestion() {}
 
+function nextQuestion() {
+  showQuestion(shuffleQuestions[currentQuestion]);
+}
+
+function showQuestion(question) {
+  questionEl.innerText = question.question;
+  question.a.b.c.d.forEach((correctAnswerEL) => {
+    var button = document.createElement("button");
+    button.innerText = answer.text;
+    button.classList.add("btn");
+    if (answer.correct) {
+      button.dataset.correct = answer.correct;
+      button.addEventListener("click", selectAnswer);
+    }
+    correctAnswerEL.appendChild(button);
+  });
+}
+
+function resetState() {
+  nextButton.classList.add("hide");
+  while (correctAnswerEL.firstChild) {
+    correctAnswerEL.removeChild(correctAnswerEL.firstChild);
+  }
+}
+
+function selectAnswer() {}
+
+function startTimer() {
+  timerInterval = setInterval(function () {
+    timeLeft -= 1;
+
+    updateTimerValue();
+
+    if (timeLeft <= 0 || currentQuestion === questions.length) {
+      endGame();
+    }
+  }, 1000);
+}
+
+function endGame() {
+  clearInterval(timerInterval);
+  if (timeLeft < 0) {
+    timeLeft = 0;
+  }
+}
+updateTimerValue();
+
+function updateTimerValue() {
+  timerEl.textContent = timeLeft;
+}
 //creating question array
 var questions = [
   {
@@ -47,7 +104,11 @@ var questions = [
     d: "None of the above",
     correct: "d",
   },
+  {
+    question: "Which keyword is NOT used to declare a variable?",
+    a: "let",
+    b: "const",
+    c: "store",
+    d: "var",
+  },
 ];
-
-//Eventlisteners
-startButton.addEventListener("click", startGame);
